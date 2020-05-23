0fbb74dc64066ed81b5d02dbac8a55756454a189
havvanuraydin <havvanuraydiin@gmail.com>
2020-05-24 00:46:24 +0200

    update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,5 +3,10 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
         //lets start
+
+        int i = 0;
+        if(i == 0){
+            System.out.println(i+5);
+        }
     }
 }
